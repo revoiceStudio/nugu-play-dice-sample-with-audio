4e82c9d8ac8607dd91081988dff81b2ad84aa06f
kiminseob <kis6473@naver.com>
2020-08-14 08:56:59 +0900

    modify port

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  SERVER_PORT: process.env.SERVER_PORT || 2000,
+  SERVER_PORT: process.env.SERVER_PORT || 3000,
   DOMAIN: "https://blog.kakaocdn.net/dn/WgIdl/btqGAPQRddZ/sKUC9phdbRIuqHstHGLsy1/tfile.mp3"
 }
